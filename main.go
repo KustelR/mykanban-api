4bb1b5fa8aadd45dd3d11ab2d8873ee8e356ae71
KustelR <dmitriysstudios2@gmail.com>
2025-02-28 11:54:40 +0400

    allowed delete in cors

--- main.go
+++ main.go
@@ -63,7 +63,7 @@ func main() {
 func cors(handler func(http.ResponseWriter, *http.Request)) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 
 		if r.Method == "OPTIONS" {
