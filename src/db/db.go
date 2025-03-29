7b7da07ebb1965ee00209a38a60f0f34a1cded31
KustelR <dmitriysstudios2@gmail.com>
2025-03-29 15:40:19 +0400

    automatic reconnect on fail every 10 seconds

--- src/db/db.go
+++ src/db/db.go
@@ -3,20 +3,35 @@ package db_driver
 import (
 	"database/sql"
 	"fmt"
+	"log"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func GetDb(connString string) *sql.DB {
-	db, err := sql.Open("mysql", connString)
-	if err != nil {
-		panic(fmt.Errorf("can't connect to database, err: %s", err))
-	}
+	var err error
+	var db *sql.DB
 
-	err = db.Ping()
-	if err != nil {
-		panic(fmt.Errorf("failed pinging database: %s", err))
+	failCount := 0
+	loopControl := true
+	for loopControl {
+		if failCount > 0 {
+			time.Sleep(time.Second * 10)
+		}
+		db, err = sql.Open("mysql", connString)
+		if err != nil {
+			failCount += 1
+			log.Printf("can't connect to database, err: %s", err)
+			continue
+		}
+		err = db.Ping()
+		if err != nil {
+			failCount += 1
+			log.Printf("failed pinging database: %s", err)
+			continue
+		}
+		loopControl = false
 	}
 	fmt.Println("Connected to MySql DB")
 	db.SetMaxOpenConns(10)
